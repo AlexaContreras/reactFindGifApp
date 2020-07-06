04ba898ead9a7897c9bcec59957dfc58c0d89326
AlexaContreras <root@Macs-MacBook-Air.local>
2020-07-06 11:05:20 -0300

    docs agregados

---
+++ docs/precache-manifest.07fc4659ffe8458d056adcacfdaba19e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef5b7f2d2ec6b8861f5670130f3a4814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be32c236fb2b8b42def",
+    "url": "/static/css/main.c61b4478.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be32c236fb2b8b42def",
+    "url": "/static/js/main.e9b0b151.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
